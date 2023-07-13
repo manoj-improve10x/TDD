ea5b4a8699642952e3200f1faa2aa3788bddcc9d
manoj <manoj.mopoori@improve10x.com>
2023-07-13 17:07:34 +0530

    completed fibonacci testcases

---
+++ fibonacci/scoringSystem/Scoring.java
@@ -0,0 +1,12 @@
+package fibonacci.scoringSystem;
+
+public class Scoring {
+    public int[] calculateScores(Object numbers) {
+        if(numbers != null && numbers.equals("A")) {
+            return new int[] {1,0,0};
+        } else if(numbers != null && numbers.equals("B")) {
+            return new int [] {0,1,0};
+        }
+        return new int[3];
+    }
+}
---
+++ fibonacci/scoringSystem/ScoringSystemTest.java
@@ -0,0 +1,49 @@
+package fibonacci.scoringSystem;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ScoringSystemTest {
+
+    @Test
+    public void nothing() {
+
+    }
+
+    @Test
+    public void givenNull_returnsAllZeros() {
+        Scoring scoring = new Scoring();
+        int [] allZeros = scoring.calculateScores(null);
+        assertArrayEquals(new int[3],allZeros);
+    }
+
+    @Test
+    public void givenEmpty_returnsAllZeros() {
+        Scoring scoring = new Scoring();
+        int [] allZeros = scoring.calculateScores("");
+        assertArrayEquals(new int[3],allZeros);
+    }
+
+    @Test
+    public void givenA_returnsOneZeroZero() {
+        Scoring scoring = new Scoring();
+        int[] onlyAScores = scoring.calculateScores("A");
+        assertArrayEquals(new int[] {1,0,0}, onlyAScores);
+    }
+
+    @Test
+    public void givenB_returnsZeroOneZero() {
+        Scoring scoring = new Scoring();
+        int[] onlyBScores = scoring.calculateScores("B");
+        assertArrayEquals(new int[] {0,1,0}, onlyBScores);
+    }
+
+    @Test
+    public void givenC_returnsZeroZeroOne() {
+        Scoring scoring = new Scoring();
+        int[] onlyCScores = scoring.calculateScores("C");
+        assertArrayEquals(new int[] {0,0,1}, onlyCScores);
+    }
+}
