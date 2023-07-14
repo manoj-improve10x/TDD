c4790691e5ebc036ea0e1b30c20bf8b7f7b2577f
manoj <manoj.mopoori@improve10x.com>
2023-07-14 14:26:06 +0530

    completed Palindrome testcases

--- fibonacci/palendrome/PalendremeTest.java
+++ fibonacci/palendrome/PalendremeTest.java
@@ -2,16 +2,24 @@ package fibonacci.palendrome;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PalendremeTest {
 
     @Test
     public void nothing() {
 
     }
-
     @Test
     public void givenZero() {
         Palendrome palendrome = new Palendrome();
         boolean number = palendrome.isNumber(0);
+        assertEquals(true,number);
+    }
+    @Test
+    public void givenTwoNumbers() {
+        Palendrome palendrome = new Palendrome();
+        boolean number = palendrome.isNumber(121);
+        assertEquals(false,number);
     }
 }
--- fibonacci/palendrome/Palendrome.java
+++ fibonacci/palendrome/Palendrome.java
@@ -2,6 +2,16 @@ package fibonacci.palendrome;
 
 public class Palendrome {
     public boolean isNumber(int number) {
+        int reverse = number;
+        int sum =0;
+        while (number > 0) {
+            reverse = number % 10;
+            sum = (sum * 10) + reverse;
+            number = number / 10;
+            }
+        if (reverse == number) {
+            return true;
+        }
         return false;
     }
 }
