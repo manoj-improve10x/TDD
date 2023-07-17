d686c5d9fbe17940387f26a736cd11fd78dec1de
manoj <manoj.mopoori@improve10x.com>
2023-07-17 15:53:07 +0530

    I completed Less Than Hundred testcases

---
+++ fibonacci/lessThenHundred/LessThenHundred.java
@@ -0,0 +1,10 @@
+package fibonacci.lessThenHundred;
+
+public class LessThenHundred {
+    public boolean isNumbersAreLessThen(int a, int b) {
+        if(a+b <= 100){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ fibonacci/lessThenHundred/LessThenHundredTest.java
@@ -0,0 +1,40 @@
+package fibonacci.lessThenHundred;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+public class LessThenHundredTest {
+
+    @Test
+    public void nothing() {
+
+    }
+
+    @Test
+    public void giveOneLessNumbers() {
+        LessThenHundred lessThenHundred = new LessThenHundred();
+        boolean result = lessThenHundred.isNumbersAreLessThen(12,13);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void giveOneLessNumberAndPlusNumber() {
+        LessThenHundred lessThenHundred = new LessThenHundred();
+        boolean result = lessThenHundred.isNumbersAreLessThen(-12,13);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void giveOneLessNumberAndPlusNumbers() {
+        LessThenHundred lessThenHundred = new LessThenHundred();
+        boolean result = lessThenHundred.isNumbersAreLessThen(12,-13);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void giveOneLessNumberAndHighNumbers() {
+        LessThenHundred lessThenHundred = new LessThenHundred();
+        boolean result = lessThenHundred.isNumbersAreLessThen(12,130);
+        assertEquals(false, result);
+    }
+}
