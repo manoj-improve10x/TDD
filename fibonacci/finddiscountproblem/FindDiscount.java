e32a040afd77fd527c1f470eb9cd30f220c3254c
manoj <manoj.mopoori@improve10x.com>
2023-07-20 12:21:55 +0530

    I completed FindDiscount testcases

--- fibonacci/finddiscountproblem/FindDiscount.java
+++ fibonacci/finddiscountproblem/FindDiscount.java
@@ -2,9 +2,9 @@ package fibonacci.finddiscountproblem;
 
 public class FindDiscount {
 
-    public int findDiscount(int price, int discount) {
-        int discountAmount = 0;
-        int finalPrice = 0;
+    public float findDiscount(float price, int discount) {
+        float discountAmount = 0;
+        float finalPrice = 0;
         if (price > 0) {
             discountAmount = price * discount / 100;
             finalPrice = price - discountAmount;
--- fibonacci/finddiscountproblem/FindDiscountTest.java
+++ fibonacci/finddiscountproblem/FindDiscountTest.java
@@ -9,56 +9,56 @@ public class FindDiscountTest {
     @Test
     public void givenDiscountZero_returnDiscountZero() {
         FindDiscount discount= new FindDiscount();
-        int totalDiscount = discount.findDiscount(0, 0);
+        float totalDiscount = discount.findDiscount(0, 0);
         assertEquals(Integer.MIN_VALUE, totalDiscount);
     }
 
     @Test
     public void givenDiscountMinus10_returnDiscountZero() {
         FindDiscount discount= new FindDiscount();
-        int totalDiscount = discount.findDiscount(0, 1);
+        float totalDiscount = discount.findDiscount(0, 1);
         assertEquals(Integer.MIN_VALUE, totalDiscount);
     }
 
     @Test
     public void givenPriceAndDiscount_returnTotalAmount() {
         FindDiscount discount= new FindDiscount();
-        int totalDiscount = discount.findDiscount(1, 0);
+        float totalDiscount = discount.findDiscount(1, 0);
         assertEquals(1, totalDiscount);
     }
 
     @Test
     public void givenPriceDiscount_returnTotalDiscountValue1() {
         FindDiscount discount= new FindDiscount();
-        int totalDiscount = discount.findDiscount(1, 1);
+        float totalDiscount = discount.findDiscount(1, 1);
         assertEquals(1, totalDiscount);
     }
 
     @Test
     public void givenPriceDiscount_returnTotalDiscountNegativeValue() {
         FindDiscount discount= new FindDiscount();
-        int totalDiscount = discount.findDiscount(-1, 0);
+        float totalDiscount = discount.findDiscount(-1, 0);
         assertEquals(Integer.MIN_VALUE, totalDiscount);
     }
 
     @Test
     public void givenNegativePriceDiscount_returnTotalDiscount() {
         FindDiscount discount= new FindDiscount();
-        int totalDiscount = discount.findDiscount(0, -1);
+        float totalDiscount = discount.findDiscount(0, -1);
         assertEquals(Integer.MIN_VALUE, totalDiscount);
     }
 
     @Test
     public void givenPriceDiscountValues_returnTotalDiscount() {
         FindDiscount discount= new FindDiscount();
-        int totalDiscount = discount.findDiscount(100, 75);
+        float totalDiscount = discount.findDiscount(100, 75);
         assertEquals(25, totalDiscount);
     }
 
     @Test
     public void givenPriceDiscountValue_returnTotalDiscount() {
         FindDiscount discount= new FindDiscount();
-        int totalDiscount = discount.findDiscount(1000, 75);
+        float totalDiscount = discount.findDiscount(1000, 75);
         assertEquals(250, totalDiscount);
     }
 }
