84143f0321586e2ef0518a0fc3a0145ec2559ba5
manoj <manoj.mopoori@improve10x.com>
2023-07-24 14:42:25 +0530

    I completed Player testcases

---
+++ fibonacci/templerun/Player.java
@@ -0,0 +1,50 @@
+package fibonacci.templerun;
+
+public class Player {
+
+        private String name;
+        private int health;
+        private int score;
+        public Player(String name) {
+            this.name = name == null ? "" : name.trim();
+            this.health = 100;
+            this.score = 0;
+        }
+
+        public Player(String name, int health) {
+            this.name = name == null ? "" : name.trim();
+            if(health < 0 || health > 100) {
+                throw new InvalidHealthException();
+            } else {
+                this.health = health;
+            }
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getHealth() {
+            return health;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public void collectCoin(Coin coin) {
+            this.score += coin.getValue();
+        }
+
+        public void run() {
+            System.out.println("Running...!");
+        }
+
+        public void jump() {
+            System.out.println( name + " jumped to avoid obstacles!");
+        }
+
+        public class InvalidHealthException extends RuntimeException{
+
+        }
+}
